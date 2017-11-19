37f95b2a0d652e79de9bb8b4286d9c73728bf786
koooge <koooooge@gmail.com>
2017-11-19 14:59:01 +0000

    Change: peco install way

--- install_golang.sh
+++ install_golang.sh
@@ -19,11 +19,10 @@ install_golang() {
 }
 
 go_get_packages() {
-  GO_CMD=golang.org/x/tools/cmd
-  go get ${GO_CMD}/godoc
-  go get ${GO_CMD}/goimports
-#  go get ${GO_CMD}/gorename
-#  go get ${GO_CMD}/guru
+  go get golang.org/x/tools/cmd/godoc
+  go get golang.org/x/tools/cmd/goimports
+#  go get golang.org/x/tools/cmd/gorename
+#  go get golang.org/x/tools/cmd/guru
 
   go get github.com/golang/lint/golint
   go get github.com/motemen/gore
@@ -34,11 +33,13 @@ go_get_packages() {
   go get github.com/rogpeppe/godef
 #  go get github.com/jstemmer/gotags
   go get github.com/k0kubun/pp
-#  go get github.com/Masterminds/glide
+  go get github.com/Masterminds/glide
 #  go get github.com/pilu/fresh
-  go get github.com/peco/peco/cmd/peco
   go get github.com/github/hub
   go get github.com/kardianos/govendor
+
+  wget -O - https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_amd64.tar.gz | tar zxf - -C /tmp/
+  cp /tmp/peco_linux_amd64/peco ${GOPATH}/bin/
 }
 
 # install_gvm
