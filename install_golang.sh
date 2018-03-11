4dd43bc5096f4fd2bfe6cee95f326d36b76a71fc
koooge <koooooge@gmail.com>
2018-03-11 13:22:25 +0000

    dont install go libs

--- install_golang.sh
+++ install_golang.sh
@@ -20,24 +20,24 @@ install_golang() {
 
 go_get_packages() {
   go get golang.org/x/tools/cmd/godoc
-  go get golang.org/x/tools/cmd/goimports
-  go get github.com/golang/dep/cmd/dep
+#  go get golang.org/x/tools/cmd/goimports
+#  go get github.com/golang/dep/cmd/dep
 #  go get golang.org/x/tools/cmd/gorename
 #  go get golang.org/x/tools/cmd/guru
 
-  go get github.com/golang/lint/golint
+#  go get github.com/golang/lint/golint
   go get github.com/motemen/gore
   go get github.com/motemen/ghq
 	echo "[ghq]" >> ~/.gitconfig
 	echo "	root = ${HOME}/ghq" >> ~/.gitconfig
-  go get github.com/nsf/gocode
-  go get github.com/rogpeppe/godef
+#  go get github.com/nsf/gocode
+#  go get github.com/rogpeppe/godef
 #  go get github.com/jstemmer/gotags
-  go get github.com/k0kubun/pp
+#  go get github.com/k0kubun/pp
 #  go get github.com/pilu/fresh
-  go get github.com/github/hub
+#  go get github.com/github/hub
 
-  wget -O - https://github.com/peco/peco/releases/download/v0.5.1/peco_linux_amd64.tar.gz | tar zxf - -C /tmp/
+  wget -O - https://github.com/peco/peco/releases/download/v0.5.2/peco_linux_amd64.tar.gz | tar zxf - -C /tmp/
   cp /tmp/peco_linux_amd64/peco ${GOPATH}/bin/
 }
 
