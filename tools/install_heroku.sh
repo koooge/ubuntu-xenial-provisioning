78a8cda3d6ebd8e146ee23669ffca1baf75b77e3
koooge <koooooge@gmail.com>
2018-05-16 14:06:54 +0000

    update

--- tools/install_heroku.sh
+++ tools/install_heroku.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
 
-sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
-curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-sudo apt-get update -qq
-sudo apt-get install -y heroku
+curl https://cli-assets.heroku.com/install-standalone.sh | sh
