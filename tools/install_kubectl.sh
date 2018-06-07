94e79d6dabe5da3185501c556bf146994bedad33
koooge <koooooge@gmail.com>
2018-06-07 16:52:50 +0900

    Add kubectl completion bash

--- tools/install_kubectl.sh
+++ tools/install_kubectl.sh
@@ -5,3 +5,5 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo bash -c 'echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list'
 sudo apt-get update -qq
 sudo apt-get install -y kubectl
+
+echo "source <(kubectl completion bash)" >> ~/.bashrc
